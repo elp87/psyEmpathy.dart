bc12062c207c172b907e2afc05952215b0688cdf
elp87 <elp1987@gmail.com>
2013-11-21 18:05:43 +0400

    Preparing page to start

--- web/psyempathy.dart
+++ web/psyempathy.dart
@@ -1,7 +1,34 @@
 import 'dart:html';
+import 'dart:convert' show JSON;
+import 'dart:async' show Future;
+
+ButtonElement okButton;
+DivElement questBlock;
 
 void main() {
+  okButton = querySelector('#ok-button');
+  questBlock = querySelector('#quest-text');
   
+  Quests.readyQuests();  
+}
+
+class Quests {
+  static List<String> texts = [];
+  
+  static Future readyQuests() {
+    var path = 'questions.json';
+    return HttpRequest.getString(path).then(_parseQuestFromJSON).then(_prepareControls);
+  }
+  
+  static _parseQuestFromJSON(String jsonString) {
+    Map quests = JSON.decode(jsonString);
+    texts = quests['quests'];
+  }
+  
+  static _prepareControls(dynamic value) {
+    questBlock.text = texts[0];
+    okButton.disabled = false;
+  }
 }
 
 
