be29a1aef353c6d70ec84918d5b20737c17bb5d1
elp87 <elp1987@gmail.com>
2013-11-22 16:17:09 +0400

    okButton click event handler

--- web/psyempathy.dart
+++ web/psyempathy.dart
@@ -5,9 +5,14 @@ import 'dart:async' show Future;
 ButtonElement okButton;
 DivElement questBlock;
 
+List<int> answerList;
+int curAnswerNumber;
+
 void main() {
   okButton = querySelector('#ok-button');
   questBlock = querySelector('#quest-text');
+  
+  okButton.onClick.listen(okButton_Click);
   
   Quests.readyQuests();  
 }
@@ -28,7 +33,24 @@ class Quests {
   static _prepareControls(dynamic value) {
     questBlock.text = texts[0];
     okButton.disabled = false;
+    answerList = [];
+    curAnswerNumber = 0;
   }
 }
 
-
+void okButton_Click(MouseEvent event) {
+  InputElement radio = querySelector("input[type='radio']:checked");
+  if (radio == null) {
+    window.alert("Не выбран вариант ответа");
+    return;
+  }
+  answerList.add(int.parse(radio.value));
+  curAnswerNumber++;
+  if (curAnswerNumber == Quests.texts.length) {
+    window.alert(answerList.toString());
+    okButton.disabled = true;
+    return;
+  }
+  questBlock.text = Quests.texts[curAnswerNumber];
+  radio.checked = false;
+}
