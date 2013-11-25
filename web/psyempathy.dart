3647baf646949f780273272176001dae6751c672
elp87 <elp1987@gmail.com>
2013-11-25 18:19:33 +0400

    Display test result in window, not with alerts

--- web/psyempathy.dart
+++ web/psyempathy.dart
@@ -5,6 +5,7 @@ import 'lib/Testing.dart';
 
 ButtonElement okButton;
 DivElement questBlock;
+DivElement testBlock;
 int curAnswerNumber;
 Testing curTest;
 
@@ -29,21 +30,72 @@ void okButton_Click(MouseEvent event) {
   curAnswerNumber++;
   if (curAnswerNumber == Quests.texts.length) {
     curTest.calcResults();
+    prepareWindowForResult();
     showResult();
-    window.alert("Ваша достоверность - ${curTest.honesty}");
-    okButton.disabled = true;
     return;
   }
   questBlock.text = Quests.texts[curAnswerNumber];
   radio.checked = false;
 }
 
+void prepareWindowForResult() {
+  okButton = null;
+  questBlock = null;
+  testBlock = querySelector('#test-block');
+  testBlock.innerHtml = '<div id="honesty-block"> Искренность ответов </div> <div id="testResult-short"> Коротко </div> <div id="testResult-long"> Развернуто </div>';
+}
+
 void showResult() {
-  if (curTest.testValue >= 82) { window.alert(curTest.descriptionsList[0].longDesc); }
-  if (curTest.testValue >= 63 && curTest.testValue <= 81) { window.alert(curTest.descriptionsList[1].longDesc); }
-  if (curTest.testValue >= 37 && curTest.testValue <= 62) { window.alert(curTest.descriptionsList[2].longDesc); }
-  if (curTest.testValue >= 12 && curTest.testValue <= 36) { window.alert(curTest.descriptionsList[3].longDesc); }
-  if (curTest.testValue <= 11) { window.alert(curTest.descriptionsList[4].longDesc); }
+  String shortResult, longResult;
+  DivElement honestyDiv = querySelector('#honesty-block');
+  honestyDiv..innerHtml = getHonestyDescription()
+            ..style.background = getHonestyBlockColor();
+            
+  DivElement shortDiv = querySelector('#testResult-short');
+  DivElement longDiv = querySelector('#testResult-long');
+    
+  if (curTest.testValue >= 82) { 
+    longResult = curTest.descriptionsList[0].longDesc;
+    shortResult = curTest.descriptionsList[0].shortDesc;
+    shortDiv.style.background = "red";
+  }
+  if (curTest.testValue >= 63 && curTest.testValue <= 81) { 
+    longResult = curTest.descriptionsList[1].longDesc;
+    shortResult = curTest.descriptionsList[1].shortDesc;
+    shortDiv.style.background = "yellow";
+  }
+  if (curTest.testValue >= 37 && curTest.testValue <= 62) { 
+    longResult = curTest.descriptionsList[2].longDesc;
+    shortResult = curTest.descriptionsList[2].shortDesc;
+    shortDiv.style.background = "green";
+  }
+  if (curTest.testValue >= 12 && curTest.testValue <= 36) { 
+    longResult = curTest.descriptionsList[3].longDesc;
+    shortResult = curTest.descriptionsList[3].shortDesc;
+    shortDiv.style.background = "yellow";
+  }
+  if (curTest.testValue <= 11) { 
+    longResult = curTest.descriptionsList[4].longDesc;
+    shortResult = curTest.descriptionsList[4].shortDesc;
+    shortDiv.style.background = "red";
+  }
+  
+  shortDiv.innerHtml = shortResult;
+  longDiv.innerHtml = longResult;
+}
+
+String getHonestyDescription() {
+  String honestyDescription = "";
+  if (curTest.honesty == 4) { honestyDescription = "Клиент был не до конца честен. В результатах теста следует сомневаться"; }
+  if (curTest.honesty >= 5) { honestyDescription = "Клиент был не честен. Результатам теста верить нельзя";  }
+  return honestyDescription;  
+}
+
+getHonestyBlockColor() {
+  String backgroundColor = "";
+  if (curTest.honesty == 4) { backgroundColor = "yellow"; }
+  if (curTest.honesty >= 5) { backgroundColor = "red";  }
+  return backgroundColor;
 }
 
 prepareTest() {
